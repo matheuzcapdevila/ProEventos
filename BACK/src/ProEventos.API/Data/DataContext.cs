b7635cfd78e622bfddb807fdb20c92f45c734a12
Matheus Camargo <matheuz.lcc@gmail.com>
2026-02-17 13:51:46 -0300

    salvando

---
+++ BACK/src/ProEventos.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        
+    }
+}
